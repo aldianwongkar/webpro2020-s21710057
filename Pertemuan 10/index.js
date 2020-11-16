cd1108055e34c12b751cd216316ec04b42197cf6
aldianwongkar <s21710057@student.unklab.ac.id>
2020-11-16 10:55:35 +0800

    Ten commit

---
+++ Pertemuan 10/index.js
@@ -0,0 +1,37 @@
+let nama ="aldi";
+//let nama = "wongkar";    //Error!!
+console.log(nama);
+
+//Scope dalam javascript
+// 1. Function scope (var)
+// 2. Block scope
+
+
+//(function tes(){
+//for(var i =0;i<10;i++){
+  //  console.log(i);
+//}
+//})();
+//tes();
+//console.log(i);
+
+//for (let i=0;i<10;i++){
+ //   console.log(i)
+//}
+//console.log(i)
+
+// 6
+// {
+//     let i;
+//     for(i = 0; i<10;i++){
+//         console.log(i)
+//     }
+// }
+
+const person = {
+    nick: "miya",
+    role: "marksman",
+}
+console.log("My nick is "+person.nick+". I playing role "+person.role+".");
+
+console.log(`My nick is ${person.nick}. I playing role ${person.role}.`);
